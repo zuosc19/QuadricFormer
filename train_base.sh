ba561284f533c0a2e766dc07602276626a477907
Xiaoyong Han <118900915+han-xy@users.noreply.github.com>
2025-06-24 21:15:22 +0800

    add_tran.sh

---
+++ train_base.sh
@@ -0,0 +1,3 @@
+torchrun --nproc_per_node 8 --nnodes 1 --node_rank 0 --master_addr 127.0.0.1 --master_port 63546 train.py \
+    --py-config config/nusc_surroundocc_sq12800.py \
+    --work-dir work_dir/sq12800/debug
